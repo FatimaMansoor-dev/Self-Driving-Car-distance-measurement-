b87da78e337920a13019bf7a3ebdd71ee102b236
Fatima Mansoor <139262595+FatimaMansoor-dev@users.noreply.github.com>
2024-09-12 23:08:56 +0500

    Add files via upload

---
+++ day3/shape_recognition.py
@@ -0,0 +1,28 @@
+import cv2
+import imutils
+
+def find_circles(frame, mask):
+    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+
+    # print(f"Number of contours found: {len(contours)}")
+
+    center = None  # Initialize center to None by default
+
+    if len(contours) > 0:
+        c = max(contours, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+
+        if M["m00"] != 0:
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        else:
+            center = (int(x), int(y))
+
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 0), -1)
+    # else:
+    #     print("No valid contours found")
+
+    return center
